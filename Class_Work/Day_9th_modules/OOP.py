c30c89160e9817a40129cb25c21e1cbf1a800f1e
Bani Goswami <banigoswamiwork@gmail.com>
2026-07-17 00:22:59 -0700

    work

--- Class_Work/Day_9th_modules/OOP.py
+++ Class_Work/Day_9th_modules/OOP.py
@@ -8,3 +8,8 @@ class Rectangle:
         def dispay(self):
             print("Length:", self.length)
             print("Width:", self.width)
+#Step 2: object creation'''obj name = object of class Rectangle
+# it gives a memory to the class Rectangle and the memory is assigned to the object name obj'''obj will work outside the class Rectangle and it can access the attributes and methods of the class Rectangle
+rect = Rectangle()
+rect.initialise(5, 10)
+rect.display_data()
